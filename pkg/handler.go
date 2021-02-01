328723a3a35bf6c7296b3f47582318c739796bd6
x33lyS <florian.lejosne@ynov.com>
2021-02-01 14:48:21 +0100

    ok

---
+++ pkg/handler.go
@@ -0,0 +1,32 @@
+package pkg
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func FaviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "assets/css/reno.ico")
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		switch r.Method {
+		case "GET":
+			t, err := template.ParseFiles("templates/index.html")
+			if err != nil {
+				InternalServerError(w, r)
+			}
+			_ = t.Execute(w, nil)
+		case "POST":
+			t, err := template.ParseFiles("templates/index.html")
+			if err != nil {
+				InternalServerError(w, r)
+			}
+			_ = t.Execute(w, nil)
+		}
+
+	} else {
+		StatusNotFound(w, r)
+	}
+}
